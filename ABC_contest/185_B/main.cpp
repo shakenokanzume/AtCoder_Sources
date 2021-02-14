8bf2b4c009778d0b859a2edcc557c87baf3cb05f
shakenokanzume-2 <to.d20174.kiko@gmail.com>
2021-02-14 19:10:20 +0900

    ABC185_B

--- ABC_contest/185_B/main.cpp
+++ ABC_contest/185_B/main.cpp
@@ -9,5 +9,39 @@ const int lim = 1000000007;
 int main(){
     ll N, M, T;
     cin >> N >> M >> T;
-    
+    // 初期状態においては、A=0, B=0として考える
+    vector<ll> A(M+1, 0), B(M+1, 0);
+    for(int i = 1; i < M+1; i++){
+        cin >> A[i] >> B[i];
+    }
+
+    // 現在のバッテリー容量を表す変数
+    ll cur_N = N;
+
+    // ループ処理開始
+    for(int i = 1; i < M+1; i++){
+        // 1つ前のカフェを出てから、今のカフェに移るまでに消費されるバッテリー
+        ll minus = A[i] - B[i-1];
+        // 今のカフェで稼ぐことができるバッテリー
+        ll plus = B[i] - A[i];
+
+        // バッテリーの消費・供給計算
+        cur_N -= minus;
+        // バッテリー残量がなくなるなら、このタイミング
+        if(cur_N <= 0){
+            cout << "No" << endl;
+            return 0;
+        }
+
+        cur_N = min(N, cur_N + plus);
+    }
+
+    // 最後のカフェを出てから、終了時間までに消費される文のバッテリー計算
+    cur_N -= (T - B[M]);
+    if(cur_N <= 0){
+        cout << "No" << endl;
+        return 0;
+    }
+    cout << "Yes" << endl;
+    return 0;
 }

1ad31f06982ab2c15cb9458324bb765af1c39462
shakenokanzume-2 <to.d20174.kiko@gmail.com>
2021-02-14 19:15:38 +0900

    ABC172_A

---
+++ ABC_contest/172_A/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+using namespace std;
+
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    int a;
+    cin >> a;
+    cout << a + a*a + a*a*a << endl;
+    return 0;
+}

c920cb96639fec4dcef4b2e968333ab00888302d
Yonekawa Taito <to.d20174.kiko@gmail.com>
2021-02-15 16:31:34 +0900

    188_B

--- ABC_contest/188_B/main.cpp
+++ ABC_contest/188_B/main.cpp
@@ -9,12 +9,31 @@ const int lim = 1000000007;
 int main(){
     int N;
     cin >> N;
-    int sum = 0;
-    vector<int> a(N), b(N);
-    rep(i, N) cin >> a[i];
-    rep(i, N) cin >> b[i];
-    rep(i, N) sum += a[i] * b[i];
-    if(sum == 0) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    // 完全2分木のトーナメント表を左右半分に割って、左半分での最強と、右半分での最強を得る
+    // firstに最大の選手iの番号iを、secondに最大の選手iのレートa[i]を入れる
+    pair<int, int> l_p, r_p;
+    l_p = make_pair(0, 0);
+    r_p = make_pair(0, 0);
+
+    rep(i, pow(2, N)){
+        int a;
+        cin >> a;
+        if(i < pow(2, N-1)){
+            if(l_p.second < a){
+                l_p.second = a;
+                l_p.first = i+1;
+            }
+        }else{
+            if(r_p.second < a){
+                r_p.second = a;
+                r_p.first = i + 1;
+            }
+        }
+    }
+    if(l_p.second < r_p.second){
+        cout << l_p.first << endl;
+    }else{
+        cout << r_p.first << endl;
+    }
     return 0;
 }

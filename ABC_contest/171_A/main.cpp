672104574356e265f5c5084a3c332a5dcdc850c7
Yonekawa Taito <to.d20174.kiko@gmail.com>
2021-02-15 16:44:27 +0900

    171_A

---
+++ ABC_contest/171_A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < n; i++)
+
+const int lim = 1000000007;
+
+int main(){
+    char val;
+    cin >> val;
+    if(97 <= val && val <= 122) cout << "a" << endl;
+    else cout << "A" << endl;
+    return 0;
+}

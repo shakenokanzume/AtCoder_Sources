3d54dc6dda3a3a1675d0512c5ea85c1c06510f77
Yonekawa Taito <to.d20174.kiko@gmail.com>
2021-07-24 12:51:55 +0900

    2021_07_24_commit

--- ABC_practice/210_C/main.cpp
+++ ABC_practice/210_C/main.cpp
@@ -9,30 +9,26 @@ const int lim = 1000000007;
 int main(){
     int N, K;
     cin >> N >> K;
-    vector<ll> c(N);
+    vector<int> c(N);
     rep(i, N) cin >> c[i];
-    // 連続区間内に含まれる色を格納する
-    set<ll> st;
     // 各色の出現回数をカウントする
-    map<ll, int> mp;
+    map<int, int> mp;
 
     // 初期化
     for(int i = 0; i <= K-1; i++){
-        st.insert(c[i]);
         mp[c[i]]++;
     }
     // 最大値を計算
-    ll mx = st.size();
-    for(int i = 1; i+K < N; i++){
+    int mx = mp.size();
+    for(int i = 0; i+K < N; i++){
         // 新しく入ってくる物に対して
         mp[c[i+K]]++;
-        st.insert(c[i+K]);
         // 範囲から出る物に対して
-        mp[c[i-1]]--;
-        if(mp[c[i-1]] <= 0) st.erase(c[i-1]);
+        mp[c[i]]--;
+        if(mp[c[i]] <= 0) mp.erase(c[i]);
 
         // 最大値をカウント
-        mx = max(mx, (ll)st.size());
+        mx = max(mx, (int)mp.size());
     }
     cout << mx << endl;
     return 0;
